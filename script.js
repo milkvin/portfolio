13c800ff117bedc053813e037d2e3f04bce2034a
milkvin <67132657+milkvin@users.noreply.github.com>
2020-10-18 17:04:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const cacheKey = "NUMBER";
- 
-           const menu = document.querySelector(".menu");
-           menu.addEventListener('click', function(event) {
-               let number = localStorage.getItem(cacheKey);
-               number++;
-               localStorage.setItem(cacheKey, number);
-           })
+function clickCounter() {
+  if(typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "Welcome, " + localStorage.clickcount + " visitors!";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
